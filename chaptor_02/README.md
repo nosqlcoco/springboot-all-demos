2026dd43c844a113a3ddc57ea2b062c2446a46ac
nosqlcoco <nosqlcoco@gmail.com>
2017-04-14 09:13:56 +0800

    application.properties配置文件详解

--- chaptor_02/README.md
+++ chaptor_02/README.md
@@ -1,1 +1,9 @@
 #### springboot之application.properties配置文件详解
+
+application.properties
+1. 是springboot默认的配置文件
+2. 应用启动时从classpath中查找是否有application.properties文件
+3. 如果没有则使用springboot提供的默认配置
+4. 如果有则加载开发者定义的application.properties文件，并覆盖内置的默认配置
+5. 使用@Value注解读取properties属性
+6. 但是静态属性无法直接使用@Value注解，需要对该属性生成非静态的setter/getter方法，并在setter方法上使用@Value注解
--- chaptor_02/src/main/java/com/nosqlcoco/chaptor02/Application.java
+++ chaptor_02/src/main/java/com/nosqlcoco/chaptor02/Application.java
@@ -6,8 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Hello world!
- *
+ * application.properties
+ * 1. 是springboot默认的配置文件
+ * 2. 应用启动时从classpath中查找是否有application.properties文件
+ * 3. 如果没有则使用springboot提供的默认配置
+ * 4. 如果有则加载开发者定义的application.properties文件，并覆盖内置的默认配置
+ * 
+ * 5. 使用@Value注解读取properties属性
+ * 6. 但是静态属性无法直接使用@Value注解，需要对该属性生成非静态的setter/getter方法，并在setter方法上使用@Value注解
+ * @author nosqlcoco
  */
 @SpringBootApplication
 public class Application {
